899cc9c8e2fe343c1a1a65986f1221319b507bac
Rozenberg <ozsh.rozenberg@mail.ru>
2022-09-30 10:49:06 +0500

    New class "about"

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -5,10 +5,14 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import commands.*;
 
 public class Bot {
 
-    TelegramBot bot;
+    public static TelegramBot bot;
+    public static long chatId;
+    public static String text;
+    public static String userName;
 
     public Bot(String token) {
         bot = new TelegramBot(token);
@@ -16,12 +20,11 @@ public class Bot {
 
     private void process(Update update )
     {
-        // Send messages
-        long chatId = update.message().chat().id();
-        String text = update.message().text();
-        String userName = update.message().chat().firstName();
+        chatId = update.message().chat().id();
+        text = update.message().text();
+        userName = update.message().chat().firstName();
         if (text.equals("/about")) {
-            SendResponse response = bot.execute(new SendMessage(chatId, "Вас интересовать не должно"));
+            About.execute();
         }
     }
 
