501454c456f36831e7d97e34aa69351d37ca2bce
Rozenberg <ozsh.rozenberg@mail.ru>
2022-10-15 14:33:20 +0500

    New class "help"

--- src/main/java/commands/About.java
+++ src/main/java/commands/About.java
@@ -5,7 +5,12 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
 public class About {
-    public static void execute(){
-        SendResponse response = Bot.bot.execute(new SendMessage(Bot.chatId, "Вас интересовать не должно"));
+
+    public About(){
+        Help.add("/about", "Получить информацию о функционале и авторах бота");
+    }
+
+    public static void execute(long chatId){
+        SendResponse response = Bot.bot.execute(new SendMessage(chatId, "Розенберг(КН-201), Гальперин(КН-201), продолжение следует"));
     }
 }
--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -10,22 +10,35 @@ import commands.*;
 public class Bot {
 
     public static TelegramBot bot;
-    public static long chatId;
-    public static String text;
-    public static String userName;
+    public static Help help;
+    public static About about;
 
     public Bot(String token) {
         bot = new TelegramBot(token);
+        help = new Help();
+        about = new About();
     }
+
 
     private void process(Update update )
     {
+        long chatId;
+        String text;
+        String userName;
+
         chatId = update.message().chat().id();
         text = update.message().text();
         userName = update.message().chat().firstName();
+
         if (text.equals("/about")) {
-            About.execute();
+            about.execute(chatId);
         }
+        if (text.equals("/help")) {
+            help.helpAll(chatId);
+        } else if (text.substring(0, 5).equals("/help")) {
+            help.helpCertain(chatId, text.substring(6, text.length()));
+        }
+
     }
 
     public void run(){
